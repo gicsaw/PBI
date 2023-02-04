25e76901d0d9602afb850135f31613459a3db745
gicsaw <gicsaw0@gmail.com>
2023-02-04 20:59:51 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ setup(name='pbi',
         author_email='gicsaw0@gmail.com',
         description='',
         scripts=['bin/align_3d.py', 'bin/cal_box.py',
-                'bin/dist_ligand.py', 'bin/fix_ligand_ref.py',
-                'bin/fix_protein.py', 'bin/gen3d.py', 'bin/pdb2pdbqt.py',
-                'bin/pdbqt2pdb_ref.py', 'bin/nwalign.py',
+                'bin/check_binding_water.py', 'bin/dist_ligand.py',
+                'bin/dock_rmsd.py', 'bin/dw_pdb.py', 'bin/filter_pdb_list.py',
+                'bin/find_mutation_pdb.py', 'bin/fix_ligand_ref.py',
+                'bin/fix_protein.py', 'bin/gen3d.py', 'bin/nwalign.py',
+                'bin/pdb2pdbqt.py', 'bin/pdbqt2pdb_ref.py',
                 'bin/split_chain.py', 'bin/split_ligand.py',
-                'bin/fix_ligand.py', 'bin/dock_rmsd.py'
+                'bin/uniprot_dict.py'
                ]
 )
 
