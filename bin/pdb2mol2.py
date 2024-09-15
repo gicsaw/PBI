655a030d76468075fe6c9d59378fe66e2550a13a
shhong <shhong@novelismlab.com>
2024-09-15 21:14:30 +0900

    mod pdb2mol2

--- bin/pdb2mol2.py
+++ bin/pdb2mol2.py
@@ -1,52 +1,6 @@
 #!/usr/bin/env python
-from openbabel import pybel
-from pbi.pdbtools import PDBtools
+from pbi.ligandtools import pdb_to_mol2
 
-
-def gen_bond_info(conect):
-    bond_info = dict()
-    for i in conect.keys():
-        bond_info[i] = dict()
-        b = conect[i]
-        for j in b:
-            if j not in bond_info[i]:
-                bond_info[i][j] = 1
-            else:
-                bond_info[i][j] += 1
-    return bond_info
-
-
-def mol2_to_kekule(mol2_line_list, bond_info):
-    mol2_line_list_kekule = list()
-    bond_check = False
-    for line in mol2_line_list:
-
-        if line == '':
-            mol2_line_list_kekule.append(line)
-            continue
-        if line.startswith('@'):
-            if line == '@<TRIPOS>BOND':
-                bond_check = 1
-            else:
-                bond_check = 0
-            mol2_line_list_kekule.append(line)
-            continue
-
-        if not bond_check:
-            mol2_line_list_kekule.append(line)
-            continue
-#        bond_idx = int(line[0:6])
-        atom1_idx = int(line[6:12])
-        atom2_idx = int(line[12:18])
-        bond_name = line[18:23]
-
-        if bond_name == '   ar':
-            bond_new = bond_info[atom1_idx][atom2_idx]
-            line_new = '%s%5d' % (line[0:18], bond_new)
-            mol2_line_list_kekule.append(line_new)
-        else:
-            mol2_line_list_kekule.append(line)
-    return mol2_line_list_kekule
 
 
 def main():
@@ -66,26 +20,8 @@ def main():
     mol2_file = args.output_file
     is_kekule = args.kekule
 
-    m_pdb = PDBtools.read_pdb_ligand(pdb_file)
-    p_keys = sorted(m_pdb.keys())
 
-    ms = pybel.readfile('pdb', pdb_file)
-    fp_out = open(mol2_file, 'w')
-
-    for model_idx, m in enumerate(ms):
-        mol2_line = m.write('mol2')
-        mol2_line_list = mol2_line.split('\n')
-
-        if is_kekule:
-            conect = m_pdb[p_keys[model_idx]][2]
-            bond_info = gen_bond_info(conect)
-            mol2_line_list_kekule = mol2_to_kekule(mol2_line_list, bond_info)
-            mol2_line_list = mol2_line_list_kekule
-        for line in mol2_line_list:
-            line += '\n'
-            fp_out.write(line)
-
-    fp_out.close()
+    pdb_to_mol2(pdb_file, mol2_file, is_kekule)
 
 
 if __name__ == "__main__":
--- pbi/ligandtools.py
+++ pbi/ligandtools.py
@@ -4,6 +4,7 @@ from openbabel import pybel
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import subprocess
+from .pdbtools import PDBtools
 
 
 def ligand_preparation(smi, neutralize, pH):
@@ -330,6 +331,76 @@ def pdbqt_to_pdb_ref(input_pdbqt_file, output_pdb_file, ref_pdb_file):
     subprocess.check_output(run_line.split(), stderr=subprocess.STDOUT,
                             universal_newlines=True)
     return
+
+
+def gen_bond_info(conect):
+    bond_info = dict()
+    for i in conect.keys():
+        bond_info[i] = dict()
+        b = conect[i]
+        for j in b:
+            if j not in bond_info[i]:
+                bond_info[i][j] = 1
+            else:
+                bond_info[i][j] += 1
+    return bond_info
+
+
+def mol2_to_kekule(mol2_line_list, bond_info):
+    mol2_line_list_kekule = list()
+    bond_check = False
+    for line in mol2_line_list:
+
+        if line == '':
+            mol2_line_list_kekule.append(line)
+            continue
+        if line.startswith('@'):
+            if line == '@<TRIPOS>BOND':
+                bond_check = 1
+            else:
+                bond_check = 0
+            mol2_line_list_kekule.append(line)
+            continue
+
+        if not bond_check:
+            mol2_line_list_kekule.append(line)
+            continue
+#        bond_idx = int(line[0:6])
+        atom1_idx = int(line[6:12])
+        atom2_idx = int(line[12:18])
+        bond_name = line[18:23]
+
+        if bond_name == '   ar':
+            bond_new = bond_info[atom1_idx][atom2_idx]
+            line_new = '%s%5d' % (line[0:18], bond_new)
+            mol2_line_list_kekule.append(line_new)
+        else:
+            mol2_line_list_kekule.append(line)
+    return mol2_line_list_kekule
+
+
+def pdb_to_mol2(pdb_file, mol2_file, is_kekule):
+    m_pdb = PDBtools.read_pdb_ligand(pdb_file)
+    p_keys = sorted(m_pdb.keys())
+
+    ms = pybel.readfile('pdb', pdb_file)
+    fp_out = open(mol2_file, 'w')
+
+    for model_idx, m in enumerate(ms):
+        mol2_line = m.write('mol2')
+        mol2_line_list = mol2_line.split('\n')
+
+        if is_kekule:
+            conect = m_pdb[p_keys[model_idx]][2]
+            bond_info = gen_bond_info(conect)
+            mol2_line_list_kekule = mol2_to_kekule(mol2_line_list, bond_info)
+            mol2_line_list = mol2_line_list_kekule
+        for line in mol2_line_list:
+            line += '\n'
+            fp_out.write(line)
+
+    fp_out.close()
+
 
 
 def read_coor_pdb(input_file, exclude_Hs=True):
